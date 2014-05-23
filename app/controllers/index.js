b4973b77b0bb83a326e8f8d479c0c4130ad56068
ffabreti <ffabreti@gmail.com>
2014-05-23 16:40:20 -0300

    bug fixes

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,22 +6,30 @@ $.tfResult.focusable=true; //needed for keypressed
 //-------------------------------------------
 $.tfResult.addEventListener("keypressed", function(e) {
     
-    if (e.keyCode==4 || e.keyCode==67) {//back key (soft and hard keyboard)
-        e.source.value = e.source.value.slice(0, -1); //chop
-        return;  
-    }
+
     //alert(e.keyCode);
     if (typeof(len)==='undefined') len = e.source.value.length;
+    
+
     //Ti.API.info('==================');
     //Ti.API.info('e.source.value: ' + e.source.value);
     if (len !== e.source.value.length) {
-         maskIdent(this);
+        //setTimeout(function(){alert(e.keyCode);}, 1);
+        if (e.keyCode==4) {//back key (soft keyboard)
+            e.source.value = e.source.value.slice(0, -1); //chop
+        } else if (e.keyCode==67) {
+            //back key (hard keyboard) 
+        }
+        else {
+            maskIdent(this);   
+        }
          len = e.source.value.length;
          e.source.setSelection(len,len);
     }
     //Ti.API.info('value: ' + e.source.value);
     //Ti.API.info('len: ' + len);
-    //Ti.API.info('e.source.value: ' + e.source.value);
+    //Ti.API.info('e.source.value: ' + e.source.value);  
+    
 });
 //-------------------------------------------
 $.tfMask.addEventListener("change", function(e){
